41b6e11dc96d64dccef687b578aa3ff5a0f0677a
W. James Dittmar <wjdittmar@gmail.com>
2024-04-17 11:32:12 -0700

    implement logout button

--- front/src/components/Logout/index.jsx
+++ front/src/components/Logout/index.jsx
@@ -1,4 +1,11 @@
+import { useNavigate } from "react-router-dom";
 const Logout = () => {
-	return <div className="logout"><span>L</span></div>;
+	const navigate = useNavigate();
+	const handleLogout = () => {
+		localStorage.removeItem("loggedUser");
+		navigate('/login');
+	};
+
+	return <div className="logout" onClick={handleLogout}><span>L</span></div>;
 };
 export default Logout;
