c4dfc68917d4f7ab450d6dbb99db388e72b93881
W. James Dittmar <wjdittmar@gmail.com>
2024-04-18 14:29:50 -0700

    add TODO for keeping track of selected page

--- front/src/components/Pagination/index.jsx
+++ front/src/components/Pagination/index.jsx
@@ -1,8 +1,13 @@
 import Button from '@mui/material/Button';
+
+// TODO: keep track of which page is being selected so that we can create
+// a separate CSS entry for the selected page
+// will probably have to update the logic to use the react router
+
 const Pagination = ({ totalPages, handlePagination }) => (
 	<div className="pagination">
 		{Array.from({ length: totalPages }, (_, index) => (
-			<Button variant="outlined" href="#outlined-buttons" key={index} onClick={() => handlePagination(index + 1)}>
+			<Button variant="outlined" key={index} onClick={() => handlePagination(index + 1)}>
 				{index + 1}
 			</Button>
 		))}
