9b54b14052c406f08ea975dab92da74633fd816b
W. James Dittmar <wjdittmar@gmail.com>
2024-04-22 14:41:07 -0700

    store authorization token in cookie rather than local variable to persist on refresh

--- front/src/services/Auth.jsx
+++ front/src/services/Auth.jsx
@@ -1,12 +1,14 @@
 
+import Cookies from "js-cookie";
 let token = null;
 
 const setToken = newToken => {
 	token = `Bearer ${newToken}`;
+	Cookies.set('authorizationToken', token, { expires: 7, secure: true });
 };
 
 const getToken = () => {
-	return token;
+	return Cookies.get('authorizationToken');
 };
 
 export default { getToken, setToken };
