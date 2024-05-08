8101b438e8ba3208ba790baa778ef3909f93556b
W. James Dittmar <wjdittmar@gmail.com>
2024-05-08 14:41:58 -0700

    create an inbox page

--- back/src/routes/message/message.js
+++ back/src/routes/message/message.js
@@ -26,7 +26,16 @@ messageRouter.post("/", verifyToken, async (request, response) => {
 	} catch (error) {
 		response.status(500).json({ error: error.message });
 	}
+});
 
+messageRouter.get("/toRecipient", verifyToken, async (request, response) => {
+	try {
+		const recipient = request.query.recipient;
+		const messages = await Message.find({ recipient: { $eq: recipient } }).populate("sender", { name: 1 }).populate("recipient", { name: 1 });
+		response.json({ messages });
+	} catch (error) {
+		response.status(500).json({ error: error.message });
+	}
 });
 
 
--- front/src/components/Messages/index.tsx
+++ front/src/components/Messages/index.tsx
@@ -1,31 +1,28 @@
 import { useState, useEffect } from "react";
-import useWebSocket from 'react-use-websocket';
-import Autocomplete from '@mui/material/Autocomplete';
-import TextField from '@mui/material/TextField';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
-
-import authService from '../../services/Auth';
-import userService from '../../services/Users';
+import useWebSocket from 'react-use-websocket';
 import storageService from '../../services/Storage';
-
+import authService from '../../services/Auth';
+import messageService from "../../services/Messages";
 const Messages = () => {
 
 	const [alertOpen, setAlertOpen] = useState(false);
 	const [sender, setSender] = useState('');
 
+	const token = authService.getToken().replace("Bearer ", "");
+
 	const handleAlertClose = (event, reason) => {
 		if (reason === 'clickaway') {
 			return;
 		}
-
 		setAlertOpen(false);
 	};
+
 
 	const handleMessage = (message) => {
 		setSender(message.data);
 		setAlertOpen(true);
-
 		// call the alert notification with the message data
 	};
 	const handleError = (event) => {
@@ -33,10 +30,17 @@ const Messages = () => {
 		// call the alert notification with the message data
 	};
 
+	const [messages, setMessages] = useState([]);
+	useEffect(() => {
+		messageService.getByRecipient(storageService.getCurrentUserID())
+			.then(messages => {
+				setMessages(messages.messages);
+			})
+			.catch(error => {
+				console.error("Error fetching messages:", error);
+			});
+	}, [alertOpen]);
 
-	// TODO: is this the correct way to pass the authorization token to the WebSockets service?
-
-	const token = authService.getToken().replace("Bearer ", "");
 	const { sendMessage } = useWebSocket(
 		`ws://${location.hostname}:8000?token=${token}`,
 		{
@@ -44,77 +48,23 @@ const Messages = () => {
 			onError: handleError
 		}
 	);
-	const [formData, setFormData] = useState({
-		recipient: '',
-		body: '',
-		subject: '',
-	});
-	const [recipients, setRecipients] = useState([]);
-
-
-	const handleSubmit = async (event) => {
-		event.preventDefault();
-		sendMessage(JSON.stringify({
-			...formData,
-			sendDate: Date.now(),
-			sender: storageService.getCurrentUserID()
-		}));
-	};
-
-	useEffect(() => {
-		userService.getAll().then(users => {
-			const restructuredUsers = users.map(user => ({
-				id: user._id,
-				label: `${user.name}`
-			}));
-			setRecipients(restructuredUsers);
-		});
-	}, []);
-
 
 	return (
 		<>
-			<h2>Inbox</h2>
-			<form className="create" onSubmit={handleSubmit}>
-				<div className="inputWrapperContainer">
-					<label htmlFor="recipient" className="required">Subject</label>
-					<Autocomplete
-						options={recipients}
-						renderInput={(params) => <TextField {...params} label="Recipient" />}
-						sx={{ flex: 1, border: 'none' }}
-						onChange={(event, newRecipient) => setFormData({ ...formData, recipient: newRecipient.id })}
-						isOptionEqualToValue={(option, value) => option.value === value.value}
-					/>
-				</div>
-				<div className="inputWrapperContainer">
-					<label htmlFor="subject" className="required">Subject</label>
-					<span className="inputWrapper">
-						<input
-							type="text"
-							id="subject"
-							name="subject"
-							enterKeyHint="next"
-							value={formData.subject}
-							onChange={(event) => setFormData({
-								...formData,
-								subject: event.target.value
-							})}
-						/>
-					</span>
-				</div>
-				<div className="inputWrapperContainer">
-					<label htmlFor="visitNotes">Body</label>
-					<span className="inputWrapper">
-						<textarea
-							id="visitNotes"
-							name="visitNotes"
-							value={formData.body}
-							onChange={(event) => setFormData({ ...formData, body: event.target.value })}
-						/>
-					</span>
-				</div>
-				<button type="submit">Submit</button>
-			</form>
+			<table>
+				<thead>
+					<tr>
+						<th>Sender</th>
+						<th>Subject</th>
+						<th>Message</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					{messages.map((message) => (
+						<tr> <td>{message.sender.name}</td> <td>{message.subject}</td> <td>{message.body}</td> </tr>))}
+				</tbody>
+			</table >
 			<Snackbar open={alertOpen} autoHideDuration={3000} onClose={handleAlertClose} anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}>
 				<Alert
 					onClose={handleAlertClose}
