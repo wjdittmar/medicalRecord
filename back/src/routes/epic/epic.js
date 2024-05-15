19037e199b16ad26824f797bf364686e8d0b7b71
W. James Dittmar <wjdittmar@gmail.com>
2024-05-15 13:29:55 -0700

    fix path to private key so that we can key an access token for epic's sandbox

--- back/src/routes/epic/epic.js
+++ back/src/routes/epic/epic.js
@@ -1,41 +1,30 @@
 const epicRouter = require("express").Router();
 const { verifyToken } = require("../../utils/middleware");
-const { sign, verify } = require("jsonwebtoken");
+const { sign } = require("jsonwebtoken");
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const axios = require("axios");
 const querystring = require("querystring");
 
-
-
 epicRouter.get("/", verifyToken, async (request, response) => {
 
-	// Define header and payload
-	const header = {
-		"alg": "RS384",
-		"typ": "JWT"
-	};
-
 	const baseUrl = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token";
-	const apiUrl = "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/";
-
-	var payload = {
-		"iss": "43eb9c1b-cc03-4718-b365-bf70b33b8dc7",
-		"sub": "43eb9c1b-cc03-4718-b365-bf70b33b8dc7",
+	const payload = {
+		"iss": "909c329d-76a1-4288-8de7-3403b546e8dd",
+		"sub": "909c329d-76a1-4288-8de7-3403b546e8dd",
 		"aud": "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token/",
 		"jti": uuidv4(),
-		"exp": Math.floor(Date.now() / 1000) + 300
+		"exp": Math.floor(Date.now() / 1000) + 300,
+		"iat": Math.floor(Date.now() / 1000)
 	};
 
-	console.log(payload);
+	const privateKey = fs.readFileSync("./privatekey.pem");
 
-	const privateKey = fs.readFileSync("../../privatekey.pem");
-	var jwtToken = sign(payload, privateKey, { header: header });
-	console.log(jwtToken);
+	const jwtToken = sign(payload, privateKey, {
+		algorithm: "RS256"
+	});
 
 	try {
-
-
 		const data = await axios.post(baseUrl, querystring.stringify({
 			grant_type: "client_credentials",
 			client_assertion_type: "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
@@ -46,8 +35,7 @@ epicRouter.get("/", verifyToken, async (request, response) => {
 			}
 		});
 		console.log(data);
-	}
-	catch (error) {
+	} catch (error) {
 		console.log(error);
 	}
 });
