3007319f8e2bd353b41d85594b971b888533d4da
W. James Dittmar <wjdittmar@gmail.com>
2024-05-20 14:52:23 -0700

    update font to inter

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,10 +6,7 @@ import Landing from "./components/Landing";
 
 const queryClient = new QueryClient();
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import '@fontsource/inter';
 
 function App() {
 
